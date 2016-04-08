e3f953e566165d863f747a17f6672d79d4c0398e
Courtney Waller <courtneycmeadows@gmail.com>
2016-04-08 00:16:46 +0000

    delete working

--- server.js
+++ server.js
@@ -35,4 +35,51 @@ app.post('/items', jsonParser, function(req, res) {
     res.status(201).json(item);
 });
 
+app.delete('/items/:id', function(req, res){
+  var itemId = parseInt(req.params.id);
+  //Check for NaN
+ if (itemId !== itemId) {
+     var responseMsg = {"message": "Your item id should be a number."};
+      res.status(400).json(responseMsg);
+  } else {
+    var deletedItem;
+    storage.items.forEach(function(value, indexInArray, itemArray){
+      if (value.id === itemId) {
+          deletedItem = value;
+        return itemArray.splice(indexInArray, 1);
+              }
+    });
+   
+    if(deletedItem !== undefined){ 
+      res.status(200).json(deletedItem);
+    } else{
+      res.status(404).json({'message':'No item with this ID.'});
+    }
+  } 
+});
+
+app.put('/items/:id', function(req, res) {
+  console.log(req);// yep, this one. 
+  var itemId = parseInt(req.params.id);
+  //Check for NaN
+  if (itemId !== itemId) {
+     var responseMsg = {"message": "Your item id should be a number."};
+      res.status(400).json(responseMsg);
+  } else {
+  var updatedItem;
+  storage.items.forEach(function(value, indexInArray, itemArray){
+   // console.log('value:' + value);
+    //console.log('item:' + itemId);
+    if (value.id=== itemId) {
+        // here, your finding the item you want to change in the array correctly, then you store it into updatedItem
+      updatedItem = value;
+      // then, here what do you want to put back into the array at this location?
+      storage.items[indexInArray] = updatedItem;
+   } 
+    res.status(200).json(updatedItem);
+   // use this for put    storage.items[indexInArray] = newObject
+});
+}
+});
+
 app.listen(process.env.PORT || 8080);
